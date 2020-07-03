2d0068910b5d656897e47dcfab880b8028b46ff2
ms-online <27732357@qq.com>
2020-07-03 18:29:17 +0800

    lesson10

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # vuecli-demo
 
-## lesson9 注册事件
+## lesson10 生命周期钩子函数
 
-### 子组件想要修改父组件属性的时候可以使用注册事件
+beforeCreate():这时实例还没有被创建，所以无法知道 data，也不能用 watch 监听；
 
-### 需要在父组件里面注册事件
+created()：这时实例已经创建，可以得到 data，调用 watch，但是页面显示空白
 
-### 注册事件：this.\$emit()
+beforeMount()：页面挂载前，此时页面显示空白，但是 render 函数被首次调用；
+
+mounted()：页面挂载了，这时可以看到内容，也可以访问 dom；
+
+beforeUpdate()：数据更新前，也就是虚拟 DOM 打补丁前；这时访问到的还是原有的 DOM；
+
+updated()：数据已经更新完毕，重新渲染页面。
+
+beforeDestroy()：离开页面之前被调用，清除定时器，或第三方的一些 DOM 结构；
+
+destryed()：实例已经完全被销毁。
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,6 +23,39 @@ export default {
       this.$emit('titleChanged', 'Yellemy');
     },
   },
+
+  // 生命周期函数（钩子函数）
+  beforeCreate() {
+    alert('这时实例还没有被创建，所以无法知道 data，也不能用 watch 监听；');
+  },
+
+  created() {
+    alert('这时实例已经创建，可以得到 data，调用 watch，但是页面显示空白');
+  },
+
+  beforeMount() {
+    alert('页面挂载前，此时页面显示空白，但是 render 函数被首次调用；');
+  },
+
+  mounted() {
+    alert('页面挂载了，这时可以看到内容，也可以访问 dom；');
+  },
+
+  beforeUpdate() {
+    alert('数据更新前，也就是虚拟 DOM 打补丁前；这时访问到的还是原有的 DOM；');
+  },
+
+  updated() {
+    alert('数据已经更新完毕，重新渲染页面。');
+  },
+
+  beforeDestroy() {
+    alert('离开页面之前被调用，清除定时器，或第三方的一些 DOM 结构；');
+  },
+
+  destroyed() {
+    alert('实例已经完全被销毁。');
+  },
 };
 </script>
 
