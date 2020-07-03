df82532819a9270cf6186f950fd317a6d84ce650
ms-online <27732357@qq.com>
2020-07-03 17:42:42 +0800

    lesson8

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Vue Components Demo',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background: grey;
+  padding: 10px;
+}
+
+h1 {
+  color: #fff;
+  text-align: center;
+}
+</style>
