a914bd5323fd72ed54e72061ca1be9a858af3c80
ms-online <27732357@qq.com>
2020-07-03 17:30:50 +0800

    lesson7

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>{{ Copyright }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Copyright: 'Copyright 2020',
+    };
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  background: grey;
+  padding: 10px;
+}
+
+p {
+  color: #fff;
+  text-align: center;
+}
+</style>
