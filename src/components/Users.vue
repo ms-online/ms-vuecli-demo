a20179c0bc19a2269646b2bed89e3cdd65c99b18
ms-online <27732357@qq.com>
2020-07-03 15:43:56 +0800

    lesson1-4

---
+++ src/components/Users.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="users">
+    <ul>
+      <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Yellemy', 'Worley', 'Lucy', 'Tracy'],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
